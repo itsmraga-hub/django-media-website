a429f778940b245a80f256cd34e9007dd330508f
Raga <ragawilliam570@gmail.com>
2023-04-24 23:30:08 +0300

    Add url path to add image

--- media/multimedia/urls.py
+++ media/multimedia/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from multimedia.views import AudioListView
+from multimedia.views import AudioListView, ImageListView, VideoListView, HomeView, AddImageView
 
 urlpatterns = [
-  path('audio/', AudioListView.as_view(), name='audio_list')
+  path('', HomeView.as_view(), name='home'),
+  path('audios', AudioListView.as_view(), name='audio_list'),
+  path('images', ImageListView.as_view(), name='image_list'),
+  path('videos', VideoListView.as_view(), name='video_list'),
+  path('images/add-image', AddImageView.as_view(), name='add_image')
 ]
