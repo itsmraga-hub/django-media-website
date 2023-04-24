12a5bad96068ee8c7310f7918fb78d3dba4951f5
Raga <ragawilliam570@gmail.com>
2023-04-25 01:46:52 +0300

    Add Media urls for dev mode and urls to add audion and video

--- media/media/urls.py
+++ media/media/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# import multimedia.urls
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('media/', include('multimedia.urls'))
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
