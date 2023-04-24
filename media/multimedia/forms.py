8c1f412cc842dcd990ce281ee5e550a85842bf4d
Raga <ragawilliam570@gmail.com>
2023-04-25 01:45:09 +0300

    Create Video and Audio Form classes

--- media/multimedia/forms.py
+++ media/multimedia/forms.py
@@ -4,3 +4,14 @@ from django import forms
 class AddImageForm(forms.Form):
     image = forms.ImageField()
     name = forms.CharField(max_length=255)
+
+
+class AddVideoForm(forms.Form):
+    file = forms.FileField()
+    title = forms.CharField(max_length=255)
+
+
+class AddAudioForm(forms.Form):
+    file = forms.FileField()
+    title = forms.CharField(max_length=255)
+
